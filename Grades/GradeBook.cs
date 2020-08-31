82adf28e4f98954f73b3191d26185d38839b187c
Jansan <jansan-26@hotmail.com>
2020-08-31 13:31:22 +0200

    AddGrade method and float grades list created

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -1,9 +1,18 @@
-﻿namespace Grades
+﻿using System.Collections.Generic;
+
+namespace Grades
 {
     internal class GradeBook
     {
         public GradeBook()
         {
         }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
     }
 }
